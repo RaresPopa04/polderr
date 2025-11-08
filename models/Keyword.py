8089f9bacf8442178ebfd8d897cbb70121b55b90
Teoneag <teodor.neagoe.climber@gmail.com>
2025-11-08 23:07:36 +0100

    fix keyword

--- models/Keyword.py
+++ models/Keyword.py
@@ -3,6 +3,5 @@ from dataclasses import dataclass
 
 @dataclass
 class Keyword:
-    def __init__(self, keyword, emb):
-        self.keyword = keyword
-        self.emb = emb
+    keyword: str
+    emb: any
